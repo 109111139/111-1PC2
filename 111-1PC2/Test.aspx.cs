64602972fa7785b6c4be425b53714e838bb77e3f
109111139 <benson910318@gmail.com>
2022-09-19 17:04:20 +0800

    fin

--- 111-1PC2/Test.aspx.cs
+++ 111-1PC2/Test.aspx.cs
@@ -12,9 +12,10 @@ namespace _111_1PC2
         protected void Page_Load(object sender, EventArgs e)
         {
             int i_In = 66377;
-            double d_Ou = 0;
+            double F = 0;
             double C = (double)i_In;
-            d_Ou = (C*9/5)+ 32;
+            F = (C*9/5)+ 32;
+            double d_Ou = F;
             Response.Write(d_Ou);
                 
         }
